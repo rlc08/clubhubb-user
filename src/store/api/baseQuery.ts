ea9b9358e78ac9518435f9dadbb62d640e8fade1
rlc08 <131688484+rlc08@users.noreply.github.com>
2024-01-26 10:46:15 +0530

    Update baseQuery.ts

--- src/store/api/baseQuery.ts
+++ src/store/api/baseQuery.ts
@@ -2,4 +2,4 @@
 
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const baseQuery = fetchBaseQuery({ baseUrl: "https://clubhub-backend-57s4.onrender.com/" }); 
+export const baseQuery = fetchBaseQuery({ baseUrl: "https://clubhubb-backend.cyclic.app/" }); 
